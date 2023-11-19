dbdfecae438f01334f4858a23761d8f27689af14
yack97 <yap30600@gmail.com>
2023-11-19 10:49:44 -0300

    conexion base de datos y botons

--- app.py
+++ app.py
@@ -1,14 +1,35 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template, request, redirect
 from flask_bootstrap import Bootstrap
+from flask_sqlalchemy import SQLAlchemy
+import os
 
 
-# Crear una instancia de la aplicación Flask
+
+# Crear una instancia de la aplicación Flask sqlite:///database/data_form1.sqlite3--sqlite:database/data_form1.sqlite3
 app = Flask(__name__)
 bootstrap = Bootstrap(app)
 app.static_folder = 'static'
 
+#app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///database/data_form1.sqlite3'
+#db = SQLAlchemy(app)
 
-# Rutas y vistasx
+basedir = os.path.abspath(os.path.dirname(__file__))
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'database/data_form1.sqlite3')
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False  # Evita la advertencia de seguimiento
+
+db = SQLAlchemy(app)
+#clases - model
+
+class InformacionForm(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    nombre = db.Column(db.String(64))
+    telefono = db.Column(db.String(20))
+    link = db.Column(db.String(64))
+    nombre_empresa = db.Column(db.String(40))
+    email = db.Column(db.String(40))
+    observaciones = db.Column(db.String(2000))
+
+# Rutas y vistas
 @app.route('/')
 def index():
     return render_template('welcome.html')
@@ -25,10 +46,6 @@ def nosotros():
 def blog():
     return render_template('blog.html')
 
-#@app.route('/contacto.html')
-#def contacto():
-#    return render_template('contacto.html')
-
 #propuestas
 
 @app.route('/propuesta1')
@@ -42,6 +59,41 @@ def propuesta2():
 @app.route('/propuesta3')
 def propuesta3():
     return render_template('propuesta3.html')
+
+#========================
+
+
+#=========================
+#base de datos 
+
+#@app.route('/create_db', methods=['POST'])
+#def create():
+#    data_save1 = data_save(nombre=request.fomr['nombre'])
+#    db.session.add
+#    db.session.commit()
+
+@app.route("/create_db", methods=["POST"])
+def create_db():
+    try:
+        nombre = request.form["nombre"]
+        telefono = request.form["telefono"]
+        link = request.form["link"]
+        nombre_empresa = request.form["nombre_empresa"]  # Ajustado al nombre correcto del formulario
+        email = request.form["email"]
+        observaciones = request.form["observaciones"]
+
+        # Guarda los datos en la base de datos
+        nuevo_registro = InformacionForm(nombre=nombre, telefono=telefono, link=link, nombre_empresa=nombre_empresa, email=email, observaciones=observaciones)
+        db.session.add(nuevo_registro)
+        db.session.commit()
+
+        return redirect("/")
+
+    except Exception as e:
+        # Maneja cualquier error, por ejemplo, si no se pueden guardar los datos
+        return f"Error: {str(e)}"
+
+
 
 if __name__ == '__main__':
     app.run()
